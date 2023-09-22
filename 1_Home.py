a4f8ac8873e513f1983aa24391a1b842f755fb02
ciro125 <ciro_menescal@hotmail.com>
2023-09-22 11:13:47 -0300

    thema

--- 1_Home.py
+++ 1_Home.py
@@ -4,8 +4,7 @@ import pandas as pd
 st.set_page_config(
     page_title="Home",
     page_icon="📽",
-    layout="wide",
-    theme="Dark"
+    layout="wide"
 )
 
 st.write("# PROJETO TERA - CLASSIFICADOR DE FILMES! 🎬")
